31414fec2e5b9773d499f291422934ca5c531a6c
Nickdem <nickdem559@inbox.ru>
2023-03-13 22:29:03 +0200

    feat: примерная разметка

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <div className="header">header(logo, pages)</div>
     <div className="form">form(input, button)</div>
     <div className="list">list(columns)</div>
+    ...
   </>
 }
 
