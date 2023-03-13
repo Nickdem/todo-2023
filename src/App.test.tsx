1cb051450b6cd02a64868190045b8a85f97fc816
Nickdem <nickdem559@inbox.ru>
2023-03-13 20:45:45 +0200

    feat: подготовка проекта

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders 123 text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const el = screen.getByTestId("lol");
+
+  expect(el).toHaveTextContent("123");
 });
